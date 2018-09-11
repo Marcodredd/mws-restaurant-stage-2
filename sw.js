16f0eece7f765f59927caae4150629b0e8fa06f8
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-09-11 11:10:45 +0100

    Modified the fetch and activate events

--- sw.js
+++ sw.js
@@ -5,20 +5,20 @@
 var staticCacheName = 'restaurant-info';
 var urlsToCache = [
   //cache all static files
-  '/',     // index.html
-  'restaurant.html',
-  'js/dbhelper.js',
-  'js/main.js',
-  'js/idb.js',
-  'js/restaurant_info.js',
-  'manifest.json',
-  'sw.js',
-  'img/',
-  'icon-192x192.png',
-  'icon-512x512.png',
-  'favicon.ico',
-  'css/styles.css',
-  'http://localhost:1337/restaurants'
+  './',     
+  './index.html',
+  './restaurant.html',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/idb.js',
+  './js/restaurant_info.js',
+  './manifest.json',
+  './sw.js',
+  './img/',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  './favicon.ico',
+  './css/styles.css',
 ];
 
 self.addEventListener('install', function(event) {
@@ -33,43 +33,28 @@ self.addEventListener('install', function(event) {
 
 //Delete old cache
 self.addEventListener('activate', function(event) {
-  // console.log("Service Worker activated");
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('restaurant-info') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
+      return Promise.all(cacheNames.map(function(thisCachName) {
+        //If this was a previous cache
+        if (thisCachName !== staticCacheName) {
+          //Delete the cached file
+          console.log('Deleting old cached files');
+          return caches.delete(thisCachName);
+        }
+      }));
     })
   );
 });
 
-//Fetch event
 self.addEventListener('fetch', function(event) {
-  // console.log("Service Worker starting fetch");
   event.respondWith(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        if (response) {
-          // console.log("data fetched from cache");
-          return response;
-        }
-        else {
-          return fetch(event.request).then(function(networkResponse) {
-            // console.log("data fetched from network", event.request.url);
-            //cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          }).catch(function(error) {
-            console.log("The fetch request resulted in an error", event.request.url, error);
-          });
-        }
-      });
-    }).catch(function(error) {
-      console.log("Fetch failed", error);
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
     })
   );
 });
